7982a2dcb2d2ade957b479f015943d94278772e2
MaheshMothkur <maheshmothkuri039@gmail.com>
2023-08-08 13:20:34 +0530

    practice

---
+++ practicegit/src/practicegit/abc.java
@@ -0,0 +1,10 @@
+package practicegit;
+
+public class abc {
+
+	public static void main(String[] args) {
+		System.out.println("practice");
+
+	}
+
+}
---
+++ practicegit/src/practicegit/def.java
@@ -0,0 +1,10 @@
+package practicegit;
+
+public class def {
+
+	public static void main(String[] args) {
+		System.out.println("output");
+
+	}
+
+}
